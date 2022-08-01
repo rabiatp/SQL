2e0427c0e17bd6dde341098c7a449d18702005f4
rabiatp <{username}@gmail.com>
2022-08-01 16:24:13 +0300

    Odev 7

---
+++ Odev7.sql
@@ -0,0 +1,20 @@
+-- ODEV 7
+
+-- SORGU 1 : film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating FROM film
+GROUP BY rating;
+
+-- SORGU 2 : film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT (*) >50
+
+-- SORGU 3 : customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+-- SORGU 4 : city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY count(city) desc
+LIMIT 1;
